12d06c716092e7c96594033a7d24674a152ba814
Leandre <leandre.blanchard@epitech.eu>
2019-09-03 12:57:37 +0900

    rename textbox -> inputbox
    textbox in dev

--- src/textbox.c
+++ src/textbox.c
@@ -3,32 +3,32 @@
 #include "LSCENE/lgameobject_types.h"
 #include "LSCENE/ltextbox.h"
 
-void ltextbox_set_font(lgameobject_t *textbox, const sfFont *font)
+void linputbox_set_font(lgameobject_t *textbox, const sfFont *font)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	obj->font = font;
 	sfText_setFont(obj->render_text, font);
 }
 
-void ltextbox_set_character_size(lgameobject_t *textbox, uint32_t size)
+void linputbox_set_character_size(lgameobject_t *textbox, uint32_t size)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	sfText_setCharacterSize(obj->render_text, size);
 }
 
-void ltextbox_set_color(lgameobject_t *textbox, sfColor color)
+void linputbox_set_color(lgameobject_t *textbox, sfColor color)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	obj->color = color;
 	sfText_setFillColor(obj->render_text, color);
 }
 
-void ltextbox_set_position(lgameobject_t *textbox, sfVector2f position)
+void linputbox_set_position(lgameobject_t *textbox, sfVector2f position)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	lgameobject_set_position(textbox, position);
 	sfText_setPosition(obj->render_text, position);
@@ -36,7 +36,7 @@ void ltextbox_set_position(lgameobject_t *textbox, sfVector2f position)
 
 static void update(lgameobject_t *textbox)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	if (lscene_time(textbox->scene) - obj->elapsed >= 1.0) {
 		obj->blink = !obj->blink;
@@ -49,7 +49,7 @@ static void update(lgameobject_t *textbox)
 			sfColor_fromRGB(obj->color.r / 2, obj->color.g / 2, obj->color.b / 2));
 	}
 	else {
-		ltextbox_set_color(textbox, obj->color);
+		linputbox_set_color(textbox, obj->color);
 		if (obj->blink && obj->selected)
 			lstr_addch(&obj->string, '|');
 		sfText_setString(obj->render_text, obj->string.i);
@@ -61,7 +61,7 @@ static void update(lgameobject_t *textbox)
 
 static void catch_event(lgameobject_t *textbox, const sfEvent *event)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 	sfFloatRect rect = sfText_getGlobalBounds(obj->render_text);
 	char value;
 
@@ -96,11 +96,11 @@ static void catch_event(lgameobject_t *textbox, const sfEvent *event)
 
 static void start(lgameobject_t *textbox)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	if (obj->font == NULL) {
 		if (textbox->scene->fonts.len != 0)
-			ltextbox_set_font(textbox, textbox->scene->fonts.arr[0].data);
+			linputbox_set_font(textbox, textbox->scene->fonts.arr[0].data);
 		else
 			fprintf(stderr, "Textbox: error: not font set\n");
 	}
@@ -108,17 +108,17 @@ static void start(lgameobject_t *textbox)
 
 static void destroy(lgameobject_t *object)
 {
-	ltextbox_t *obj = (ltextbox_t *)object;
+	linputbox_t *obj = (linputbox_t *)object;
 
 	lstr_destroy(&obj->string);
 	lstr_destroy(&obj->placeholder);
 	sfText_destroy(obj->render_text);
 }
 
-lgameobject_t *ltextbox_create(sfVector2f position, const char *placeholder,
+lgameobject_t *linputbox_create(sfVector2f position, const char *placeholder,
 			       uint32_t character_size, sfColor color)
 {
-	ltextbox_t *obj = calloc(1, sizeof(*obj));
+	linputbox_t *obj = calloc(1, sizeof(*obj));
 
 	if (obj == NULL || lgameobject_create(&obj->base_object, "textbox") == -1)
 		return (NULL);
@@ -134,10 +134,10 @@ lgameobject_t *ltextbox_create(sfVector2f position, const char *placeholder,
 	lgameobject_subscribe(&obj->base_object, sfEvtMouseButtonReleased);
 	lgameobject_subscribe(&obj->base_object, sfEvtKeyPressed);
 	lgameobject_subscribe(&obj->base_object, sfEvtTextEntered);
-	ltextbox_set_position((lgameobject_t *)obj, position);
-	ltextbox_set_character_size((lgameobject_t *)obj, character_size);
-	ltextbox_set_color((lgameobject_t *)obj, color);
-	obj->base_object.type = LTEXTBOX_TYPE;
+	linputbox_set_position((lgameobject_t *)obj, position);
+	linputbox_set_character_size((lgameobject_t *)obj, character_size);
+	linputbox_set_color((lgameobject_t *)obj, color);
+	obj->base_object.type = LINPUTBOX_TYPE;
 	obj->base_object.start = &start;
 	obj->base_object.update = &update;
 	obj->base_object.catch_event = &catch_event;
@@ -145,9 +145,9 @@ lgameobject_t *ltextbox_create(sfVector2f position, const char *placeholder,
 	return ((lgameobject_t *)obj);
 }
 
-const lstr_t *ltextbox_get_string(lgameobject_t *textbox)
+const lstr_t *linputbox_get_string(lgameobject_t *textbox)
 {
-	ltextbox_t *obj = (ltextbox_t *)textbox;
+	linputbox_t *obj = (linputbox_t *)textbox;
 
 	return (&obj->string);
 }
